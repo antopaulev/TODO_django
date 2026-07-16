ed48083d4d074684395011d598ee5434d1f1495d
antopaulev <antopaul2022@gmail.com>
2026-07-16 14:19:24 +0530

    now working on todo apps

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from todo.models import Task
+
 # Create your views here.
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create()
+    return HttpResponse('the form is submited')  
